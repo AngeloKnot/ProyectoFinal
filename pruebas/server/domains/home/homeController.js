980d8f8a34c1397c97830e81141f05f6b086e17a
adolfo <adolfo9angelo@gmail.com>
2022-11-17 22:33:57 -0600

    primner comit

---
+++ pruebas/server/domains/home/homeController.js
@@ -0,0 +1,26 @@
+// Cargando las variables de entorno
+import configKeys from '../../config/configKeys';
+
+// Creando los Actions Methods
+// GET "/"
+// GET "/index"
+const home = (req, res) => {
+    // 1. Generando el view-model
+    const viewModel = {
+        title: 'Express',
+        author: '💞Adolfo Angel Lopez Martinez 💞',
+    };
+
+    // 2. Madamos a generar la vista con el Template Engine
+    res.render('home/home', viewModel);
+};
+
+const about = (req, res) => {
+    const viewModel = {
+        appVersion: configKeys.appVersion,
+    };
+    res.render('home/about', viewModel);
+};
+
+// Exportando el Controlador
+export default { home, about };
